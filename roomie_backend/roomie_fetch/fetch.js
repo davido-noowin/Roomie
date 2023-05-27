2db5981ca67372fa257e64c39341ee7c15e6994f
bnguyen700 <bnguyen700@student.cccd.edu>
2023-05-27 16:39:23 -0700

    Update fetch.js

--- roomie_backend/roomie_fetch/fetch.js
+++ roomie_backend/roomie_fetch/fetch.js
@@ -1,6 +1,8 @@
-function grabData(data)
+// Fetch the accomodation data from user
+function grabAccommodationData(data)
 {
     fetch("roomAccommodations", "GET")
-
+    .then( res => console.log(res))
+    .catch( err => console.log(err))
     data.preventDefault()
 }
