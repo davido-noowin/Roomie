1fc33da2d1c680ae79540e39f1de70248a0f65fb
David Nguyen <davido.nguyen1212@gmail.com>
2023-05-27 14:37:02 -0700

    revert changes to aws name in settings

--- roomie_backend/roomie_backend/settings.py
+++ roomie_backend/roomie_backend/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'roomie_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'roomiedatabase',
+        'NAME': 'RoomieAWS',
         'USER': 'admin',
         'PASSWORD': "password",
         'HOST': "roomiedatabase.ckyamljcxjnh.us-east-2.rds.amazonaws.com",
