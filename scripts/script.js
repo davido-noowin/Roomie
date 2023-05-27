50b4c1c7250d3e22df7c1626662566b13b44da07
PeterNguyen4 <peternguyen644@gmail.com>
2023-05-27 15:16:22 -0700

    add carousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,25 @@
-function load() {
-  document.addEventListener("DOMContentLoaded", function() {
-    const carouselItems = document.querySelectorAll(".carousel-item");
-    const prevBtn = document.querySelector(".prev-btn");
-    const nextBtn = document.querySelector(".next-btn");
-    let currentIndex = 0;
-  
-    buttons.forEach(button => {
-      button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides")
-      })
-    })
-  }
-}
+const buttons = document.querySelectorAll('[data-carousel-button]');
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+  const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+  const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+  if(newIndex < 0) newIndex = slides.children.length -1;
+  if(newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+  })
+});
+
+document.getElementById('policies').addEventListener('click', () => {
+  document.getElementsByClassName('popContainer').style.display = 'block';
+});
+
+document.getElementById('closePopupBtn').addEventListener('click', () => {
+  document.getElementsByClassName('popupContainer').style.display = 'none';
+});
+
