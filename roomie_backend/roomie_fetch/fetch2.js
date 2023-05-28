4df85bd564403c9705db001b6d3342bfe87bc2a8
David Nguyen <davido.nguyen1212@gmail.com>
2023-05-27 19:08:32 -0700

    change fetch2 file

--- roomie_backend/roomie_fetch/fetch2.js
+++ roomie_backend/roomie_fetch/fetch2.js
@@ -5,7 +5,31 @@ $.ajax({
     success: function(response) {
       // Handle the response from the server
       console.log(response);
+
+      const keys = Object.keys(response)
+      console.log(keys)
       // Do something with the room information
+
+    },
+    error: function(xhr, status, error) {
+      // Handle error
+      console.log(error);
+    }
+  });
+
+
+  $.ajax({
+    url: 'http://3.133.161.88:8000/roomAccomodations/',  // URL of the Django view
+    type: 'GET',
+    dataType: 'json',
+    success: function(response) {
+      // Handle the response from the server
+      console.log(response);
+
+      const accomodations = Object.keys(response)
+      console.log(accomodations)
+      // Do something with the room information
+
     },
     error: function(xhr, status, error) {
       // Handle error
