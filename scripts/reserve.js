fed96479b91ce0575af980889091c73ff39936a6
PeterNguyen4 <peternguyen644@gmail.com>
2023-05-27 22:01:40 -0700

    fix icon

--- scripts/reserve.js
+++ scripts/reserve.js
@@ -1,94 +1,89 @@
-const daysTag = document.querySelector(".days"),
+const daysTag = document.querySelector(".days");
 currentDate = document.querySelector(".current-date");
 prevNextIcon = document.querySelectorAll(".icons span");
 
-// getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
 
-// storing full name of all months in array
-const months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-    let liTag = "";
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+  let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+  let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
-        // To find active day
-        if (i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear())
-        {
-          let day = i;
-        }
-        else 
-        {
-          i;
-        }
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+
+    if (i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear()) {
+      liTag += `<li class="${isToday}">${i}</li>`;
+    } else {
+      let currentDate = new Date(currYear, currMonth, i);
+      let today = new Date();
+      if (currentDate < today) {
+        liTag += `<li class="inactive">${i}</li>`;
+      } else {
         liTag += `<li class="${isToday}">${i}</li>`;
+      }
     }
+  }
 
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-    console.log(daysTag);
-    daysTag.innerHTML = liTag;
-}
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
+  currentDate.innerText = `${months[currMonth]} ${currYear}`;
+  daysTag.innerHTML = liTag;
+};
+
 renderCalendar();
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
-    });
+prevNextIcon.forEach(icon => {
+  icon.addEventListener("click", () => {
+    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+    if (currMonth < 0 || currMonth > 11) {
+      date = new Date(currYear, currMonth, new Date().getDate());
+      currYear = date.getFullYear();
+      currMonth = date.getMonth();
+    } else {
+      date = new Date();
+    }
+    renderCalendar();
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const checkboxes = document.querySelectorAll('.filter-checkbox');
-  const radios = document.querySelectorAll('.filter-radio');
+document.addEventListener("DOMContentLoaded", function() {
+  const checkboxes = document.querySelectorAll(".filter-checkbox");
+  const radios = document.querySelectorAll(".filter-radio");
 
   checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', applyFilters);
+    checkbox.addEventListener("change", applyFilters);
   });
 
   radios.forEach(function(radio) {
-    radio.addEventListener('click', applyFilters);
+    radio.addEventListener("click", applyFilters);
   });
 
   function applyFilters() {
-    const selectedCategories = getSelectedValues('.filter-checkbox');
-    const selectedRating = getSelectedValue('.filter-radio');
+    const selectedCategories = getSelectedValues(".filter-checkbox");
+    const selectedRating = getSelectedValue(".filter-radio");
 
-    const items = document.querySelectorAll('.item');
+    const items = document.querySelectorAll(".item");
     items.forEach(function(item) {
       const category = item.dataset.category;
       const rating = item.dataset.rating;
 
       if ((selectedCategories.length === 0 || selectedCategories.includes(category)) &&
-          (selectedRating === '' || rating === selectedRating)) {
-        item.style.display = 'block';
+          (selectedRating === "" || rating === selectedRating)) {
+        item.style.display = "block";
       } else {
-        item.style.display = 'none';
+        item.style.display = "none";
       }
     });
   }
@@ -96,17 +91,15 @@ document.addEventListener('DOMContentLoaded', function() {
   function getSelectedValues(selector) {
     const selectedValues = [];
     const elements = document.querySelectorAll(selector);
-    //console.log(elements)
     elements.forEach(function(element) {
       if (element.checked) {
         selectedValues.push(element.value);
-        //console.log(element.value)
       }
     });
 
     return selectedValues;
   }
-  // Grabbing selected radio
+
   function getSelectedValue(selector) {
     const elements = document.querySelectorAll(selector);
 
@@ -116,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    return '';
+    return "";
   }
 
   function getCalendarDate() {
@@ -127,15 +120,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const selectedDate = parseInt(date.innerText);
         let selectedMonth = newDate.getMonth() + 1;
         const selectedYear = newDate.getFullYear();
-        
+
         console.log(`Selected Date: ${selectedDate}`);
         console.log(`Selected Month: ${selectedMonth}`);
         console.log(`Selected Year: ${selectedYear}`);
-      }
-    )}
-  )}
+      });
+    });
+  }
+
   getCalendarDate();
-  //let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                    // && currYear === new Date().getFullYear() ? "active" : "";
-  //console.log(isToday);
 });
