b88e0ca1f0a1590f1c2a3720da284b7f19111aee
David Nguyen <davido.nguyen1212@gmail.com>
2023-06-14 17:36:58 -0700

    clean up carousel for front page

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
-const buttons = document.querySelectorAll('[data-carousel-button]');
+const buttons = document.querySelectorAll('.carousel-button[data-carousel-button]');
 buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    
-  const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-  const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+    const carousel = button.closest('.carousel');
+    const slides = carousel.querySelectorAll('.slide');
 
-  const activeSlide = slides.querySelector("[data-active]");
-  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-  if(newIndex < 0) newIndex = slides.children.length -1;
-  if(newIndex >= slides.children.length) newIndex = 0;
+    const activeSlide = carousel.querySelector('.slide[data-active]');
+    let newIndex = Array.from(slides).indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.length - 1;
+    if (newIndex >= slides.length) newIndex = 0;
 
-  slides.children[newIndex].dataset.active = true;
-  delete activeSlide.dataset.active;
-  })
+    activeSlide.removeAttribute('data-active');
+    slides[newIndex].setAttribute('data-active', 'true');
+  });
 });
-
 function openPopup() {
   document.getElementById('popup').classList.add("open-popup");
 }
