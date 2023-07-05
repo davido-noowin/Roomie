bd6a6774f420c6712c1d50cedfab581515e832f6
David Nguyen <davido.nguyen1212@gmail.com>
2023-07-04 17:06:22 -0700

    create basic shell of website with hello world

--- roomie/main/views.py
+++ roomie/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+def home(response:HttpResponse) -> HttpResponse:
+    return render(response, 'main/home.html')
