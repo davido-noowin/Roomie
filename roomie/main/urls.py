d4a741014d97a7ddbc29d9cb26c2facede231e07
David Nguyen <davido.nguyen1212@gmail.com>
2023-08-13 22:49:14 -0700

    front end transfer

--- roomie/main/urls.py
+++ roomie/main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("reserve/", views.reserve, name="reserve"),
+    path("my-rooms/", views.myrooms, name="my-rooms"),
+    path("about/", views.about, name="about"),
+    path("account/", views.account, name="account")
 ]
--- roomie/main/views.py
+++ roomie/main/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 def home(response:HttpResponse) -> HttpResponse:
-    return render(response, 'main/home.html')
+    return render(response, 'main/index.html', {})
+
+
+def reserve(response:HttpResponse) -> HttpResponse:
+    return render(response, 'main/reserve1.html', {})
+
+
+def myrooms(response:HttpResponse) -> HttpResponse:
+    return render(response, 'main/my-rooms.html', {})
+
+
+def about(response:HttpResponse) -> HttpResponse:
+    return render(response, 'main/about.html', {})
+
+
+def account(response:HttpResponse) -> HttpResponse:
+    return render(response, 'main/account.html', {})
